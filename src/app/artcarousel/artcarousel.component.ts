1f804c0786e1a5a4a487372cf8d80a6050d073aa
Brad <brmccr@gmail.com>
2019-11-19 15:34:27 -0500

    commit

---
+++ src/app/artcarousel/artcarousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artcarousel',
+  templateUrl: './artcarousel.component.html',
+  styleUrls: ['./artcarousel.component.css']
+})
+export class ArtcarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/artdisplay/artdisplay.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { arts } from '../arts';
+
+@Component({
+  selector: 'app-artdisplay',
+  templateUrl: './artdisplay.component.html',
+  styleUrls: ['./artdisplay.component.css']
+})
+export class ArtdisplayComponent implements OnInit {
+  arts = arts;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
